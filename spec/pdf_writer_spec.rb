e1533c05ddfd0d54cd5b18654b60401426bf2a45
Kunal Das <kunaldasece@gmail.com>
2014-08-22 15:28:58 +0530

    Create pdf_writer_spec.rb

---
+++ spec/pdf_writer_spec.rb
@@ -0,0 +1,18 @@
+require "spec_helper"
+require_relative "../lib/plugins/pdf_writer.rb"
+require_relative "../lib/user.rb"
+#require_relative "../lib/file_exporter.rb"
+
+describe "PDFWriter" do
+	let(:user) {User.new(["Kunal", "22", "9945401604"])}
+	let(:pdf_file) { PDFWriter.new(user)}
+	subject { pdf_file }
+
+	it "formats the user's data" do
+		expect(pdf_file.formatter).to eql("Name: Kunal, Age: 22, Contact: 9945401604")
+	end
+
+	it "creates a file with user's name" do
+		expect(pdf_file.filename).to eql("Kunal.pdf")
+	end
+end
