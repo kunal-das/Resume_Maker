f0341591a1f87b1cb60bc467f70af6a52e461686
Kunal Das <kunaldasece@gmail.com>
2014-08-22 15:37:13 +0530

    Create csv_writer.rb

---
+++ lib/plugins/csv_writer.rb
@@ -0,0 +1,27 @@
+require "csv"
+require_relative "../file_exporter.rb"
+
+class CSVWriter < FileExporter
+	
+	#def initialize(user_data)
+	#	@user_data = user_data
+		#register_plugin("csv", "CSVWriter")
+	#end
+
+	register_plugin("csv", "CSVWriter")
+
+	def formatter
+		"#{file.get_user_details_for_csv}".split(/,/)
+	end
+
+	def filename
+		"#{file.get_user_details[:name]}.csv"
+	end
+
+	def write_to_file
+		CSV.open(filename, "a+") do |csv|
+			csv << file.headings.split(/,/)
+			csv << formatter
+		end
+	end
+end
